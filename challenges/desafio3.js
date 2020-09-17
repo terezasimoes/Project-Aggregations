06572ab2c03ebe9f84b832cf78ecb18d37ba55c2
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-09-17 16:30:22 -0300

    desafio 4

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -7,5 +7,5 @@ db.movies.aggregate([
   },
   { $project: { _id: 0, titulo: "$title", avaliado: "$rated", notaIMDB: "$imdb.rating",
   votosIMDB: "$imdb.votes", ano: "$year"}},
-  {$sorte: {ano: -1, notaIMDB: -1, titulo: 1}}
+  {$sort: {ano: -1, notaIMDB: -1, titulo: 1}}
 ]);
